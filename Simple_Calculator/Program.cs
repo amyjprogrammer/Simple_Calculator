1cf5df615eff9e29839b4b5e41c12bd9d6714f05
amyjprogrammer <amyjprogrammer@gmail.com>
2021-08-19 21:47:30 -0400

    Add project files.

---
+++ Simple_Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adding two numbers together.");
+            Console.Write("Please enter your first number: ");
+            double firstNum = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please enter your second number: ");
+            double secondNum = Convert.ToDouble(Console.ReadLine());
+
+            double answer = secondNum + firstNum;
+
+            Console.WriteLine("The answer is {0:0.00}", answer);
+        }
+    }
+}
