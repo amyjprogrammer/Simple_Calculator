d13d76178fb65ba6bc788933588bd75b798b3146
amyjprogrammer <amyjprogrammer@gmail.com>
2021-08-22 18:13:08 -0400

    Learning Switch, While and Do While Loops

--- Simple_Calculator/Program.cs
+++ Simple_Calculator/Program.cs
@@ -6,6 +6,10 @@ namespace Simple_Calculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(GetDay(0));
+            Console.WriteLine(LearnLoop(1));
+            Console.WriteLine(DoWhileLoop(11));
+
             Console.Write("Please enter your first number: ");
             double firstNum = Convert.ToDouble(Console.ReadLine());
 
@@ -32,6 +36,62 @@ namespace Simple_Calculator
                 Console.WriteLine("Please use *, +, - or / for the operator");
             }
             
+        }
+        //Switch Statement
+        static string GetDay(int dayNum)
+        {
+            string dayName;
+            switch (dayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Invalid Day Number";
+                    break;
+            }
+            return dayName;
+        }
+
+        //while loop
+        static int LearnLoop(int num)
+        {
+            while (num <= 5)
+            {
+                Console.WriteLine(num);
+                num++;
+                                
+            }
+            return num; //this prints out 1,2,3,4,5,6 6 is included, because that is the last number the while loop checked
+        }
+
+        //do while loop
+        static int DoWhileLoop (int numb)
+        {
+            do
+            {
+                Console.WriteLine(numb); //prints out 11
+                numb++;
+            } while (numb <= 10);
+            return numb; //prints out 12, because the do while loop added 1
         }
     }
 }
